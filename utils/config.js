edd2d2f147087d6a999dd09b8ab7d168eeee6ccc
nknrw <nknrw@yandex.ru>
2022-12-11 01:03:58 +0300

    feat: правки 2

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const {
-  NODE_ENV, PORT = 4000, DB_URL = 'mongodb://localhost:27017/moviesdb', JWT_SECRET,
+  NODE_ENV, PORT = 3000, DB_URL = 'mongodb://localhost:27017/moviesdb', JWT_SECRET,
 } = process.env;
 
 module.exports = {
